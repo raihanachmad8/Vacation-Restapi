27ff8964e2487670359d3d52849db2393cc95f95
raihanachmad8 <raihanachmad8@gmail.com>
2024-08-29 10:53:32 +0700

    feat: kanban team model add partial role and permssion

--- src/models/kanban-team.model.ts
+++ src/models/kanban-team.model.ts
@@ -1,8 +1,11 @@
+import { AccessType, KanbanRole } from "@prisma/client";
 import { UserModel } from "./user";
 
 export class KanbanTeamModel {
   team_id: string;
   board_id: string;
+  role: KanbanRole;
+  permissions: AccessType;
   user: UserModel;
   created_at: Date;
   updated_at: Date;
@@ -10,6 +13,8 @@ export class KanbanTeamModel {
     const team = new KanbanTeamModel();
     partial.team_id && (team.team_id = partial.team_id);
     partial.board_id && (team.board_id = partial.board_id);
+    partial.role && (team.role = partial.role);
+    partial.permissions && (team.permissions = partial.permissions);
     partial.User && (team.user = await UserModel.toJson(partial.User));
     partial.created_at && (team.created_at = partial.created_at);
     partial.updated_at && (team.updated_at = partial.updated_at);
